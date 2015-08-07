9a684de545500c6d48e4075f518b72f41edd2c2c
mariusbjornor <marius.bjornor@gmx.com>
2015-08-07 15:36:14 +0200

    Added meta in Popup return

--- src/MariusB/ItemID/main.php
+++ src/MariusB/ItemID/main.php
@@ -1,5 +1,7 @@
 <?php
+
 namespace MariusB\ItemID;
+
 use pocketmine\item\Item;
 use pocketmine\event\Listener;
 use pocketmine\plugin\PluginBase;
@@ -7,22 +9,27 @@ use pocketmine\event\player\PlayerItemHeldEvent;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 use pocketmine\Player;
+
 class Main extends PluginBase implements Listener{
+
 public function onEnable(){
 $this->getServer()->getPluginManager()->registerEvents($this, $this);
 $this->getLogger()->info("ItemID is enabled");
 }
+
 public function onDisable(){
 $this->getLogger()->info("ItemID is Disabled");
 }
+
 public function onItemHeld(PlayerItemHeldEvent $event){
 $item = $event->getItem();
 $player = $event->getPlayer();
 if($player->hasPermission('item.info')){
-$player->sendPopup($item->getName()." §eID: ".$item->getId(), 2); // More info about item that is held
+$player->sendPopup($item->getName()." §eID: ".$item->getId().":".$item->getDamage(), 2); // More info about item that is held
 return true;
 }
 }
+
 public function onCommand(CommandSender $sender, Command $command, $label, array $args){
     if(strtolower($command->getName()) === "item"){
          if ($sender instanceof Player){
@@ -30,7 +37,7 @@ public function onCommand(CommandSender $sender, Command $command, $label, array
              if (count($args) <1) {
                 $inHandid = $sender->getInventory()->getItemInHand()->getId();
                 $inHandname = $sender->getInventory()->getItemInHand()->getName();
-                $sender->sendMessage("[ItemID]You are holding $inHandname ID $inHandid");
+                $sender->sendMessage("[ItemID]You are holding $inHandname ID $inHandid meta $inHandmeta");
                 return true;}
              else {$sender->sendMessage("[ItemID]what?what?");
               return true;}
@@ -38,5 +45,6 @@ public function onCommand(CommandSender $sender, Command $command, $label, array
            }
       return true;}
     } 
+
     return true;}    
 }
